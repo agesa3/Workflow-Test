ea40a10da7f9dcc37b10bab2331549eec2b3cbc6
agesa3 <collinsagesa2000@gmail.com>
2022-06-24 10:48:48 +0300

    project setup

---
+++ app/src/main/java/com/agesadev/githubworkflows/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.agesadev.githubworkflows
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
